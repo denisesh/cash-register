6d2333079a64738a007a14cbf33f4bc02cc21fd8
denisesh <denisesh@gmail.com>
2014-12-17 06:38:36 -0800

    first commit

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,26 @@
 class CashRegister
-  def total
-    0
+  def initialize
+    @total = 0
+    @pay = 0
   end
+
+  def total
+    @total
+  end
+
+  def purchase(item_price)
+     @total += item_price
+  end
+
+  def pay(payment_amt)
+    change = payment_amt - @total
+
+    if payment_amt > total
+      @total = 0
+    else
+      @total = @total - payment_amt
+    end
+    change
+  end
+
 end
