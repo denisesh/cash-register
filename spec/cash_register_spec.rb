b52a66b6e4ed7bda724684baad1162ec85a6e895
denisesh <denisesh@gmail.com>
2014-12-17 06:41:10 -0800

    couple of tests

--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -11,3 +11,36 @@ describe CashRegister do
     end
   end
 end
+
+
+  describe "purchase" do
+    it "adds the total" do
+      cash_register = CashRegister.new
+      cash_register.purchase(1.23)
+
+      expect(cash_register.total).to eq(1.23)
+    end
+  end
+
+  describe "total method" do
+    it "returns how much is owed" do
+
+    end
+  end
+
+  describe "pay method" do
+    it "takes only one floating number"
+    end
+  end
+
+  describe "pay method" do
+    it "returns how much change is given"
+    end
+  end
+
+  # * There is a purchase method that takes a floating number and adds that to the total.
+  # * There is a total method that returns how much is owed.
+  # * There is a pay method that takes one floating number for how much is paid. It should return how much change is given.
+  # * When the Cash Register is initialized is should begin with a starting balance of 0.
+
+end
